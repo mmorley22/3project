5d67861aef4ad359872b7e73aa3955e44d3c55db
mmorley22 <eugeniamor@aol.com>
2015-01-25 07:17:20 -0500

    update

--- run_analysis.R
+++ run_analysis.R
@@ -3,34 +3,33 @@
   install.packages("dplyr")
   install.packages("reshape2")
   library(dplyr)
-  library(reshape2)
-# assume all of the files are in the same directory-- no subdirectories
+  library(reshape2)# assume all of the files are in the same directory-- no subdirectories
 # program to take raw data and create a tidy data set
-    lb <- read.table("features.txt")
-    testxdata <- read.table("X_test.txt")
+  lb <- read.table("features.txt")
+  testxdata <- read.table("X_test.txt")
 #    ## remove troublesome characters from names of variables
 #    ## use gsub to remove ( ) _ and ,
-    nam <- gsub("\\(","", lb[,2])
-    nam <- gsub("\\)","", nam)
-    nam <- gsub("\\-","", nam)
-    nam <- gsub(",","", nam)
-    names(testxdata) <- nam
+  nam <- gsub("\\(","", lb[,2])
+  nam <- gsub("\\)","", nam)
+  nam <- gsub("\\-","", nam)
+  nam <- gsub(",","", nam)
+  names(testxdata) <- nam
 # read in rest of data
-    trainxdata <- read.table("X_train.txt")
-    names(trainxdata) <- nam
-    subjecttrain <- read.table("subject_train.txt")
-    subjecttest <- read.table("subject_test.txt")
-    testydata <- read.table("Y_test.txt")
-    trainydata <- read.table("Y_train.txt")
-    activityL <- read.table("activity_labels.txt")
+  trainxdata <- read.table("X_train.txt")
+  names(trainxdata) <- nam
+  subjecttrain <- read.table("subject_train.txt")
+  subjecttest <- read.table("subject_test.txt")
+  testydata <- read.table("Y_test.txt")
+  trainydata <- read.table("Y_train.txt")
+  activityL <- read.table("activity_labels.txt")
 # keep position information on the y and merge activity labels with activity codes
 #  in y lista is just the original position
-    lista <- 1:length(testydata$V1)
-    testydata2 <- cbind(lista, testydata)
-    testy <- arrange(merge(testydata2, activityL, by.x = "V1", by.y ="V1"), lista)
-    lista <- 1:length(trainydata$V1)
-    trainydata2 <- cbind(lista, trainydata)
-    trainy <- arrange(merge(trainydata2, activityL, by.x="V1", by.y = "V1"), lista)
+  lista <- 1:length(testydata$V1)
+  testydata2 <- cbind(lista, testydata)
+  testy <- arrange(merge(testydata2, activityL, by.x = "V1", by.y ="V1"), lista)
+  lista <- 1:length(trainydata$V1)
+  trainydata2 <- cbind(lista, trainydata)
+  trainy <- arrange(merge(trainydata2, activityL, by.x="V1", by.y = "V1"), lista)
 # change names of subject variable to subject
   subjecttest <- rename(subjecttest, subject = V1)
   subjecttrain <- rename(subjecttrain, subject = V1)
@@ -54,6 +53,8 @@
 # meltdata  and then use dcast to find means by activity and subject
   meltdata <- melt(allf, id = c("subject", "activity"), measure.vars = 1:79)
   d <- dcast(meltdata, subject + activity ~ variable, mean)
-# write the tidy data set very long narrow data set
+# tidy up the data create a long narrow data set with melt
+# then write the data to tidy.txt
   d2 <- melt(d, id=c("subject", "activity"), measure.vars = 3:81)
+  d2 <- rename(d2, measurement_type= variable, mean= value)
   write.table(d2, "tidy.txt", row.name = FALSE)
